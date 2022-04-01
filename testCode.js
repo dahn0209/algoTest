18fdd54ce3b768f1a4dc2b0437875ca93805af03
dahn0209 <dahn0209@gmail.com>
2022-04-01 00:57:58 -0400

    first commit

---
+++ testCode.js
@@ -0,0 +1,29 @@
+function solution(A) {
+    let mark = new Set();
+    let curLength = 0;
+    let consecutiveSeq = 1;
+    let allSeqLength = [];
+
+    for(let i = 0; i < A.length; i++) {
+        let num = A[i];
+        if (!mark.has(num) && mark.size < 2) {
+            curLength++;
+            mark.add(num);
+        } else if (!mark.has(num) && mark.size === 2) {
+            // save previous seq
+            allSeqLength.push(curLength);
+            curLength = consecutiveSeq + 1;
+            mark = new Set([A[i - 1], num]);
+            consecutiveSeq = 1;
+        } else {
+            curLength++;
+            consecutiveSeq = num === A[i - 1] ? consecutiveSeq + 1 : 1;
+        }
+    }
+
+    allSeqLength.push(curLength);
+
+    return Math.max(...allSeqLength);
+}
+
+console.log(solution([1,2,3,2,3]))
