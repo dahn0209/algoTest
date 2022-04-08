af9ab1f7185067048158cae2f4fd39cadff2505a
dahn0209 <dahn0209@gmail.com>
2022-04-08 02:22:20 -0400

    heap

--- lastStoneWeight.js
+++ lastStoneWeight.js
@@ -1,13 +1,8 @@
 var lastStoneWeight = function(stones) {
 
-    if(stones.length===0){
-        return 0
-    }
     if(stones.length===1){
         return stones[0]
     }
-
-
     for(let i=stones.length-1;i>=0;i--){
         stones.sort((a,b)=>a-b);
         let firstMax=stones.pop();
@@ -16,20 +11,16 @@ var lastStoneWeight = function(stones) {
             let newWeight=firstMax-secondMax;
             stones.push(Math.abs(newWeight))
         }
-        console.log(stones)
         if(stones.length===1){
             break
         }
-
     }
-    console.log('result=>',stones)
     if(stones.length===0){
         return 0
     }else{
         return stones[0]
     }
 };
-
 
 
 
@@ -67,4 +58,40 @@ var lastStoneWeight = function(stones) {
     }else{
         return stones[0]
     }
+};
+
+
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeight = function (stones) {
+    const maxWeight = Math.max(...stones);
+    const buckets = new Array(maxWeight + 1).fill(0);
+    for (const weight of stones) {
+        buckets[weight]++;
+    }
+
+    let biggestWeight = 0,
+        currentWeight = maxWeight;
+    while (currentWeight > 0) {
+        if (buckets[currentWeight] === 0) {
+            currentWeight--;
+        } else if (biggestWeight === 0) {
+            buckets[currentWeight] %= 2;
+            if (buckets[currentWeight] === 1) {
+                biggestWeight = currentWeight;
+            }
+            currentWeight--;
+        } else {
+            buckets[currentWeight]--;
+            if (biggestWeight - currentWeight <= currentWeight) {
+                buckets[biggestWeight - currentWeight]++;
+                biggestWeight = 0;
+            } else {
+                biggestWeight -= currentWeight;
+            }
+        }
+    }
+    return biggestWeight;
 };
