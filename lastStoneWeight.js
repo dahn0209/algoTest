ca744c56d2a5d1bef681efb41d9e216aafbce12a
dahn0209 <dahn0209@gmail.com>
2022-04-08 02:10:36 -0400

    morecodes

--- lastStoneWeight.js
+++ lastStoneWeight.js
@@ -29,3 +29,42 @@ var lastStoneWeight = function(stones) {
         return stones[0]
     }
 };
+
+
+
+
+
+var lastStoneWeight = function(stones) {
+
+    if(stones.length===0){
+        return 0
+    }
+    if(stones.length===1){
+        return stones[0]
+    }
+
+
+
+    for(let i=stones.length-1;i>=0;i--){
+        let firstMax=Math.max(...stones)
+        let firstMaxIdx=stones.indexOf(firstMax);
+        stones.splice(firstMaxIdx,1);
+        let secondMax=Math.max(...stones)
+        let secondMaxIdx=stones.indexOf(secondMax);
+        stones.splice(secondMaxIdx,1);
+        if(firstMax!==secondMax){
+            let newWeight=firstMax-secondMax;
+            stones.push(Math.abs(newWeight))
+        }
+        if(stones.length===1){
+            break
+        }
+
+    }
+
+    if(stones.length===0){
+        return 0
+    }else{
+        return stones[0]
+    }
+};
