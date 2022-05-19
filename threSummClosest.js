f98c65caa02f9c81864e4e221922a6de4f78952c
dahn0209 <dahn0209@gmail.com>
2022-05-19 18:50:42 -0400

    updates

--- threSummClosest.js
+++ threSummClosest.js
@@ -47,3 +47,31 @@ var threeSumClosest = function(nums, target) {
     return closest
 
 };
+
+
+
+var threeSumClosest = function(nums, target) {
+    nums.sort((a,b)=>a-b)
+
+
+    let ans=nums[0]+nums[1]+nums[2];
+    for(let i=0;i<nums.length-2;i++){
+        let start=i+1;
+        let end=nums.length-1;
+        while(start<end){
+            let sum=nums[i]+nums[start]+nums[end];
+            if(sum>target){
+                end--
+            }else{
+                start++;
+            }
+
+            if(Math.abs(target-sum)<Math.abs(target-ans)){
+                ans=sum
+            }
+        }
+
+    }
+    return ans
+
+};
