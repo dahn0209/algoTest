dbaacbfe803252fa296519032a6c2bbdf2052459
dahn0209 <dahn0209@gmail.com>
2022-05-26 03:10:56 -0400

    added more code

--- nextPermutation.js
+++ nextPermutation.js
@@ -36,3 +36,25 @@ var nextPermutation = function(nums) {
     }
     return nums
 };
+
+
+var nextPermutation = function(nums) {
+        let i=nums.length-2;
+    while(nums[i]>=nums[i+1]){
+        i--
+    }
+    if(i>=0){
+        let j=nums.length-1;
+        while(nums[i]>=nums[j]){
+            j--
+        }
+        [nums[i], nums[j]]=[nums[j],nums[i]]
+    }
+    let left=i+1
+    let right=nums.length-1;
+    while(left<right){
+        [nums[left],nums[right]]=[nums[right],nums[left]];
+        left++;
+        right--;
+    }
+};
