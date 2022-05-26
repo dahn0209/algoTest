5571e367c7f879116827b43312832467d76de137
dahn0209 <dahn0209@gmail.com>
2022-05-26 18:59:30 -0400

    second

--- findFirstAndLastPostionOfElementInSortedArray.js
+++ findFirstAndLastPostionOfElementInSortedArray.js
@@ -29,3 +29,46 @@ var searchRange = function(nums, target) {
         return result
     }
 };
+
+
+var searchRange = function(nums, target) {
+
+    if(nums.length==0){
+        return [-1,-1]
+    }
+
+    let result=[];
+    let left=0;
+    let right=nums.length-1;
+    let lo=-1;
+    let hi=-1;
+
+    while(left<=right){
+
+        const mid=Math.floor((left+right)/2);
+console.log('target=>',target,'midFirst=>',mid,'nums[mid]=>',nums[mid])
+       if(target<=nums[mid]){
+            right=mid-1;
+           console.log('right 1loop=>',right)
+        }else left=mid+1;   console.log('left 1loop=>',left)
+
+    }
+    lo=left;
+    console.log('lo=>',lo)
+    left=0;
+    right=nums.length-1;
+
+     while(left<=right){
+
+        const mid=Math.floor((left+right)/2);
+         console.log('target=>',target,'midSec=>',mid,'nums[mid]=>',nums[mid])
+       if(target<nums[mid]){
+            right=mid-1;
+        }else left=mid+1;
+    }
+    hi=right;
+    console.log('hi=>',hi)
+
+    return lo<=hi ? [lo,hi] : [-1,-1]
+
+};
