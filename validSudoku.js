22362249491a52063fb0db760db708d0c5a284cb
dahn0209 <dahn0209@gmail.com>
2022-05-28 22:16:20 -0400

    third solution

--- validSudoku.js
+++ validSudoku.js
@@ -59,3 +59,69 @@ var isValidSudoku = function(board) {
     }
     return true
 };
+
+
+var isValidSudoku = function(board) {
+
+    //differet set for rows, columns, and subbox////
+    let rows=[];
+    let columns=[];
+    let boxes=[];
+    for(let i=0;i<board.length;i++){
+        rows.push(new Set());
+        columns.push(new Set());
+        boxes.push(new Set());
+    }
+
+    for(let i=0;i<board.length;i++){
+        for(let j=0;j<board[i].length;j++){
+             const cell=board[i][j];
+            if(cell==='.'){
+                continue
+            }
+            const boxNum=3 * Math.floor(i/3)+ Math.floor(j/3)
+            if(rows[i].has(cell)|| columns[j].has(cell)|| boxes[boxNum].has(cell)){
+                return false
+            }else{
+                rows[i].add(cell);
+                columns[j].add(cell);
+                boxes[boxNum].add(cell)
+            }
+        }
+    }
+
+    return true
+};
+
+
+var isValidSudoku = function(board) {
+
+    //differet set for rows, columns, and subbox////
+   const set=new Set()
+
+    for(let i=0;i<board.length;i++){
+        for(let j=0;j<board[i].length;j++){
+             const cell=board[i][j];
+            if(cell==='.'){
+                continue
+            }
+            const boxNum=3* Math.floor(i/3)+Math.floor(j/3)
+            const row=`row: ${i}. value ${cell}` //if value in same row colums or box twice,  string will be the same
+            const col=`col: ${j}. value ${cell}`
+            const box=`box: ${boxNum}. value ${cell}`
+            ///if there's duplicate, then false
+            if(set.has(row) || set.has(col) || set.has(box)){
+                return false;
+            }else{
+                set.add(row);
+                set.add(col);
+                set.add(box);
+            }
+
+        }
+    }
+
+    return true
+};
+
+
