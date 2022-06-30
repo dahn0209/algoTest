e538dd64c719db75db7621c9e7a0fbdecf2ab6be
dahn0209 <dahn0209@gmail.com>
2022-06-30 04:35:41 -0400

    changes

--- plusOne.js
+++ plusOne.js
@@ -12,3 +12,19 @@ var plusOne = function(digits) {
     digits.unshift(1);
     return digits
 };
+
+
+var plusOne = function(digits) {
+
+    for(let i=digits.length-1;i>=0;i--){
+        if(digits[i]<9){
+            digits[i]=digits[i]+1
+            return digits
+        }else{//49  if current digit is 9
+            digits[i]=0
+        }
+    }
+    //99->100//
+    digits.unshift(1);
+    return digits
+};
