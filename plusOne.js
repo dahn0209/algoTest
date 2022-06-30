acfab19a3626ce029f8c3578d2f872202c7ddef9
dahn0209 <dahn0209@gmail.com>
2022-06-30 04:31:44 -0400

    plusOne

---
+++ plusOne.js
@@ -0,0 +1,14 @@
+var plusOne = function(digits) {
+
+    for(let i=digits.length-1;i>=0;i--){
+        digits[i]++;
+        if(digits[i]<10){
+            return digits
+        }else{//49
+            digits[i]=0
+        }
+    }
+    //99->100//
+    digits.unshift(1);
+    return digits
+};
