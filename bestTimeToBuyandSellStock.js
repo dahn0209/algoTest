4e483cdd1344abee9286589d7ea5a3fc7733ee5d
dahn0209 <dahn0209@gmail.com>
2022-07-19 04:03:53 -0400

    correct one

--- bestTimeToBuyandSellStock.js
+++ bestTimeToBuyandSellStock.js
@@ -55,3 +55,22 @@ var maxProfit = function(prices) {
     return Math.max(...differentProfitArr)
 
 };
+
+
+
+///correct one///
+
+var maxProfit = function(prices) {
+    let buy=prices[0];
+    prices[0]=0;
+    let profit=0;
+    for(let i=1;i<prices.length;i++){
+        if(buy>prices[i]){
+            buy=prices[i];
+            prices[i]=0;
+        }else{
+            profit=Math.max(prices[i]-buy,profit)
+        }
+    }
+    return profit
+};
