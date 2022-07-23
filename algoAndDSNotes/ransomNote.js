48255a44943187e933df14fa8bbdb1523c9067c8
dahn0209 <dahn0209@gmail.com>
2022-07-23 03:32:06 -0400

    optimal solution

--- algoAndDSNotes/ransomNote.js
+++ algoAndDSNotes/ransomNote.js
@@ -31,3 +31,21 @@ var canConstruct = function(ransomNote, magazine) {
 
    return result
 };
+
+
+var canConstruct = function(ransomNote, magazine) {
+        if (ransomNote.length > magazine.length) { return false; }
+    var ransomNoteArr = ransomNote.split('');
+
+    var oldMagazineLength = magazine.length;
+    ransomNoteArr.forEach(function(item,index){
+        magazine = magazine.replace(item,'');
+    });
+
+    if(oldMagazineLength === magazine.length +ransomNoteArr.length) {
+        return true;
+    } else {
+        return false;
+    }
+
+};
