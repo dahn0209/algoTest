12320ced59c7c9fdb1226ae84cd6fd30bbe67897
dahn0209 <dahn0209@gmail.com>
2022-07-31 02:08:36 -0400

    changes in group anagram

--- groupAnagrams.js
+++ groupAnagrams.js
@@ -23,3 +23,24 @@ var groupAnagrams = function(strs) {
 
     return (Object.values(newObj))
 };
+
+
+
+var groupAnagrams = function(strs) {
+
+
+    let obj={};
+    let newSortedStrs=strs.map(word=>word.split('').sort().join(''))
+
+    let newObj={}
+
+    for(let i=0;i<strs.length;i++){
+        if(!newObj[newSortedStrs[i]]){
+            newObj[newSortedStrs[i]]=[strs[i]]
+        }else{
+            newObj[newSortedStrs[i]].push(strs[i])
+        }
+    }
+
+    return (Object.values(newObj))
+};
