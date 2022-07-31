828a7c6378edaf19d80ff135a56b01ce2c84b8ce
dahn0209 <dahn0209@gmail.com>
2022-07-31 02:05:49 -0400

    groupAnagram

---
+++ groupAnagrams.js
@@ -0,0 +1,25 @@
+var groupAnagrams = function(strs) {
+
+
+    let obj={};
+    let newSortedStrs=[]
+
+    for(let eachStr of strs){
+        let eachStrSplit=eachStr.split('');
+        eachStrSplit.sort();
+        let newStr=eachStrSplit.join('')
+        newSortedStrs.push(newStr)
+    }
+
+    let newObj={}
+
+    for(let i=0;i<strs.length;i++){
+        if(!newObj[newSortedStrs[i]]){
+            newObj[newSortedStrs[i]]=[strs[i]]
+        }else{
+            newObj[newSortedStrs[i]].push(strs[i])
+        }
+    }
+
+    return (Object.values(newObj))
+};
