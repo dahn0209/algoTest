6f0e9b1232d3683371459282bc5f3592401f62e8
dahn0209 <dahn0209@gmail.com>
2022-08-28 02:14:49 -0400

    more

--- algoAndDSNotes/containsDuplicatesII.js
+++ algoAndDSNotes/containsDuplicatesII.js
@@ -13,3 +13,23 @@ var containsNearbyDuplicate = function(nums, k) {
     }
     return false
 };
+
+
+var containsNearbyDuplicate = function(nums, k) {
+
+    let numsMap=new Map();
+
+    for(let i=0;i<nums.length;i++){
+        let num=nums[i];
+
+        if(numsMap.has(num)){
+            const j=numsMap.get(num);
+            if(Math.abs(j-i)<=k){
+                return true
+            }
+        }
+
+        numsMap.set(num,i)
+    }
+    return false
+};
