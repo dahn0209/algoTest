6d61c82e7c580526d6fff15e197d99b4d5a9686e
dahn0209 <dahn0209@gmail.com>
2022-09-02 03:52:59 -0400

    more edit

--- algoAndDSNotes/excelSheetColumnTitle.js
+++ algoAndDSNotes/excelSheetColumnTitle.js
@@ -15,3 +15,27 @@ var getChar = function (num) {
   var start = 'A'.charCodeAt(0);
   return String.fromCharCode(start + num - 1);
 };
+
+
+
+var convertToTitle = function(columnNumber) {
+    let result='';
+    let key=['Z','A','B','C','D','E','F','G','H','I','J','K',
+             'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y'];
+
+    if(columnNumber<=0){
+        return result
+    }
+
+    while(columnNumber>0){
+        let place=columnNumber%26;
+        console.log('place=>',place)
+        console.log('key[place]=>',key[place],'result=>',result)
+        result=key[place]+result;
+        console.log('columnNumber=>',columnNumber,'place=>',place)
+
+        columnNumber=place===0 ? Math.floor(columnNumber/26)-1: Math.floor(columnNumber/26)
+    }
+    return result
+
+};
