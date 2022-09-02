2de97f39c81daffe14b77b5af1695a4e4a955114
dahn0209 <dahn0209@gmail.com>
2022-09-02 04:29:38 -0400

    more edits

--- algoAndDSNotes/excelSheetColumnTitle.js
+++ algoAndDSNotes/excelSheetColumnTitle.js
@@ -28,13 +28,29 @@ var convertToTitle = function(columnNumber) {
     }
 
     while(columnNumber>0){
-        let place=columnNumber%26;
+        let place=columnNumber%26; ////columunNumber-->27 if 27%26---> place=1
+                                   ////columnNumber-->1  if 1%26---> place =1
         console.log('place=>',place)
         console.log('key[place]=>',key[place],'result=>',result)
-        result=key[place]+result;
+        result=key[place]+result;//key[place]=A + result=''    newResult=>A
+                                 ///columnNumber-->1. key[place]=A +result='A'  newResult=AA
         console.log('columnNumber=>',columnNumber,'place=>',place)
+        console.log('NewResult=>',result)
+        console.log('columnNumber=>',columnNumber)
 
-        columnNumber=place===0 ? Math.floor(columnNumber/26)-1: Math.floor(columnNumber/26)
+        // if(place===0){
+        //     columnNumber=Math.floor(columnNumber/26)-1      ////this is for when columnNumber is 26 because 26 will make place=0
+        // }else{
+        //     columnNumber=Math.floor(columnNumber/26)
+        // }
+        /////////OR///////
+        columnNumber=place===0  ? Math.floor(columnNumber/26)-1: Math.floor(columnNumber/26)
+        ////////OR//////
+         // place===0  ? columnNumber=Math.floor(columnNumber/26)-1: columnNumber=Math.floor(columnNumber/26)
+
+///since place!==0 b/c columnNumber=> 1 and  place=> 1,then columnNumber=Math.floor(columnNumber=27/26) -->columNumber now 1
+//since place!==0 b/c columnNumber=> 1 place=> 1,then columberNumber=Math.floor(columnNumber=1/26)-->columnNUmber now 0
+////since columnNumber=0, then youre outside loop
     }
     return result
 
