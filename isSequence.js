d5307a216108da513f15641c277ae5c1a939d671
dahn0209 <dahn0209@gmail.com>
2022-09-07 19:09:31 -0400

    edits

--- isSequence.js
+++ isSequence.js
@@ -34,3 +34,31 @@ var isSubsequence = function(s, t) {
     return true
 
 };
+
+
+var isSubsequence = function(s, t) {
+
+    if(s.length>t.length){
+        return false
+    }
+
+    let left=0;
+    let sLeft=0;
+    let right=t.length;
+
+    while(left<right){
+        if(s[sLeft]!==t[left]){
+            left++
+        }else if(s[sLeft]===t[left]){
+            left++;
+            sLeft++
+
+        }
+    }
+    if(s.length===sLeft){
+        return true
+    }else{
+        return false
+    }
+
+};
