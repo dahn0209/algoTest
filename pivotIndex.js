15614623ea8b74bd3a86ce1f0a3ca9daf05f2377
dahn0209 <dahn0209@gmail.com>
2022-09-07 03:15:57 -0400

    updates

--- pivotIndex.js
+++ pivotIndex.js
@@ -1,3 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ [1,7,3,6,5,6]
+ leftSum=0+1+7+8
+ 28-(leftSum=0)-(nums[0]=1)!==0
+ 28-(leftSum=1)-(nums[1]=7)!==1
+ 28-(leftSum=8)-(nums[2]=3)!==8
+ 28-(leftSum=11)-(nums[0]=6)===11
+ */
 var pivotIndex = function(nums) {
 
     let sum=0;
