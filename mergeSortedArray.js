60a96d580ff7f4d21f91cf73bb7afe481e3f86ff
David Ahn <dahn0209@gmail.com>
2022-09-23 21:03:27 -0400

    'changes'

--- mergeSortedArray.js
+++ mergeSortedArray.js
@@ -6,3 +6,26 @@ var merge = function(nums1, m, nums2, n) {
     }
     nums1.sort((a,b)=>a-b)
 };
+
+
+var merge = function(nums1, m, nums2, n) {
+    
+   let first=m-1;
+   let second= n-1
+   let i=m+n-1
+
+   while(second>=0){
+        let firstVal=nums1[first];
+        let secondVal=nums2[second];
+
+        if(firstVal>secondVal){
+            nums1[i]=firstVal
+            i--;
+            first--
+        }else{
+            nums1[i]=secondVal;
+            i--;
+            second--;
+        }
+   }
+};
