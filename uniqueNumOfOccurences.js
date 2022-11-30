47b9adf6f97bdcd53b9f37ffa11b8ffcce7212df
dahn0209 <dahn0209@gmail.com>
2022-11-30 02:32:59 -0500

    uniqueNumofOccurances

---
+++ uniqueNumOfOccurences.js
@@ -0,0 +1,22 @@
+var uniqueOccurrences = function(arr) {
+    
+    let obj={}
+    
+    for(let eachNum of arr){
+        if((eachNum in obj)){
+            obj[eachNum]++
+        }else{
+            obj[eachNum]=1;
+        }
+    }
+        
+    let value=(Object.values(obj));
+    
+    
+    for(let i=0;i<value.length;i++){
+        if(value.slice(i+1).includes(value[i])){
+            return false
+        }
+    }
+    return true
+};
