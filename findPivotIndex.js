b1f2327c8f9f3be678b0302f260b696c2accb458
dahn0209 <dahn0209@gmail.com>
2023-01-11 21:06:17 -0500

    findPivotIndex

---
+++ findPivotIndex.js
@@ -0,0 +1,58 @@
+var pivotIndex = function(nums) {
+
+    if(nums.length===1){
+        return 0
+    }
+
+    let leftSum=null;
+    let rightSum=0;
+    let indexArr=[]
+
+    for(let i=nums.length-1;i>=0;i--){
+
+        for(let eachNum of nums.slice(i+1)){
+            rightSum+=eachNum
+        }
+
+        for(let eachNum of nums.slice(0,i)){
+            leftSum+=eachNum
+        }
+
+
+      if(rightSum!==leftSum){
+          rightSum=null;
+          leftSum=0;   
+      }
+    
+      if(rightSum===leftSum 
+      &&(rightSum!==null)&&(leftSum!==null)
+      ){
+        indexArr.push(i)
+
+      }
+    }
+    if(indexArr.length===0){
+        return(-1)
+    }
+    return(indexArr[indexArr.length-1])
+};
+
+
+
+///better time and space//
+
+var pivotIndex = function(nums) {
+    
+    let sum=0;
+    for(let eachNum of nums){
+        sum+=eachNum
+    }
+    let leftSum=0;
+    for(let i=0;i<nums.length;i++){
+        if(sum-leftSum-nums[i]===leftSum){
+            return i
+        }
+        leftSum+=nums[i]
+    }
+    return -1
+};
