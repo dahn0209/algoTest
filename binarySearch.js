254987a7eb1bf573362a21236b1494b72b90c3ef
dahn0209 <dahn0209@gmail.com>
2023-01-27 01:38:03 -0500

    binary search

--- binarySearch.js
+++ binarySearch.js
@@ -61,3 +61,30 @@ var search = function(nums, target) {
     }
     return -1
 };
+
+
+var search = function(nums, target) {
+
+    
+
+    let left=0;
+    let right=nums.length-1
+
+
+    while(left<=right){
+        let middle=Math.floor((left+right)/2);
+        if(nums[middle]===target){
+            return middle
+        }
+        else if(target<nums[middle]){
+            right=middle-1;
+
+        }
+        
+        else{
+            left=middle+1
+        }
+    }
+
+    return -1
+};
