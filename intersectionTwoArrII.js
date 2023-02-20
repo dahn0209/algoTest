19c9f0766be9b073d9eca4c1fe1545e3328a803e
dahn0209 <dahn0209@gmail.com>
2023-02-20 02:09:12 -0500

    another way

--- intersectionTwoArrII.js
+++ intersectionTwoArrII.js
@@ -21,3 +21,28 @@ var intersect = function(nums1, nums2) {
     return(result)
     
 };
+
+
+var intersect = function(nums1, nums2) {
+
+    ////hash map for nums 1;
+    let obj1={};
+    let result=[];
+    ///make hashmap for nums1
+    for(let eachNums of nums1){
+        if(!(obj1[eachNums])){
+            obj1[eachNums]=1
+        }else{
+            obj1[eachNums]++
+        }
+    }
+    ///lop through nums2 and see if hashmap has count of value
+    for(let eachNum of nums2){
+        if(obj1[eachNum]>0){
+        result.push(eachNum);
+        obj1[eachNum]--;
+        }
+    }
+    return(result)
+    
+};
