300a293485657af06911429e3d0d788a4f3f68d3
dahn0209 <dahn0209@gmail.com>
2023-02-20 03:08:06 -0500

    best time to buy and sell

--- bestTimeToBuyandSellStock.js
+++ bestTimeToBuyandSellStock.js
@@ -88,3 +88,24 @@ var maxProfit = function(prices) {
     return max
 };
 
+
+
+
+var maxProfit = function(prices) {
+
+    let sellIdx=prices.length-1;
+    let profit=0;
+
+    for(let buyIdx=prices.length-1;buyIdx>=0;buyIdx--){
+        let buyVal=prices[buyIdx];
+        let sellVal=prices[sellIdx];
+
+        if(buyVal-sellVal>=0){
+            sellIdx=buyIdx;
+        }else{
+            let price=sellVal-buyVal;
+            profit=Math.max(profit,price);
+        }
+    }
+    return profit;
+};
