af0b081ae3dab99e5464d8fac194f8c3d7e9940d
dahn0209 <dahn0209@gmail.com>
2023-02-21 21:40:51 -0500

    using flat()

--- reshapeTheMatrix.js
+++ reshapeTheMatrix.js
@@ -24,3 +24,27 @@ var matrixReshape = function(mat, r, c) {
     //finally, return the answer matrix
     return answer
     };
+
+
+    var matrixReshape = function(mat, r, c) {
+    
+        //First, calculate the numer of elelemts in the original matrix and compare to the propsed matrix 
+        //If the numenr fo elements are the same, contine, otherwise, reutnr the orginal matrix
+        let rows=mat.length;
+        let columns=mat[0].length
+        if(rows*columns !== r*c){
+            return mat;
+        }
+        //second, creat an empty array to hol all the elements
+        let longList=mat.flat();
+        //third, create an empy array to hold the answer;
+        let answer=[];
+        
+        
+        for(let i=0;i<longList.length;i+=c){
+            answer.push(longList.slice(i,i+c))
+        }
+        ///fifith, slice the element in that array by the length of the colums in the propposed array.Add the slices to the answer array.
+        //finally, return the answer matrix
+        return answer
+        };
