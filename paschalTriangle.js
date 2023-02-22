3c80c030284067b7dd0af6246ce5ad42845733a0
dahn0209 <dahn0209@gmail.com>
2023-02-22 03:52:07 -0500

    another gnerate

--- paschalTriangle.js
+++ paschalTriangle.js
@@ -20,9 +20,30 @@ var generate = function(numRows) {
         }
     
         return(paschalTriangle)
-    
-    
-    
+  
         
+    };
+
+    var generate = function(numRows) {
+        if(numRows<1) return []
+    
+        if(numRows===1) return [[1]];
+    
+        const triangle=[[1]];
+    
+        for(let i=1;i<numRows;i++){
+            let prevRow=triangle[i-1];
+            let currentRow=[];
+            currentRow.push(1);
+    
+            for(let j=1;j<prevRow.length;j++){
+                currentRow[j]=prevRow[j]+prevRow[j-1];
+            }
+            currentRow.push(1);
+    
+            triangle.push(currentRow);
+        }
+        
+        return triangle
         
     };
