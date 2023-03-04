a2404c4ea52a332f129c18ef35bd3d4c4240c499
dahn0209 <dahn0209@gmail.com>
2023-03-03 21:45:58 -0500

    queue

---
+++ algoAndDSNotes/implementQueueUsingStacks.js
@@ -0,0 +1,73 @@
+var MyQueue = function() {
+    ///[]
+    ///[1]
+    /////[1,2]
+    this.pushStack=[];
+    this.popStack=[]
+    
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.pushStack.push(x)    
+};
+
+MyQueue.prototype._transfer=function(){
+    while(this.pushStack.length>0){
+        this.popStack.push(this.pushStack.pop())
+    }
+}
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    if(this.empty()){
+        return null
+    };
+
+    if(this.popStack.length===0){
+        this._transfer()
+    }
+
+    return this.popStack.pop()
+    
+};
+
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+
+    if(this.empty()){
+        return null
+    }
+
+    if(this.popStack.length===0){
+        this._transfer()
+    }
+
+    return this.popStack[this.popStack.length-1]
+    
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return this.pushStack.length===0 && this.popStack.length===0
+    
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
