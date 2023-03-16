49e014352b3965fbf69cc371e2ea462a9764eb06
dahn0209 <dahn0209@gmail.com>
2023-03-16 19:02:30 -0400

    plus minus

---
+++ hackerRank/Plus_Minus.js
@@ -0,0 +1,59 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr) {
+    // Write your code here
+    let positive=0;
+    let negative=0;
+    let zero=0;
+    
+    for(let eachNum of arr){
+        if(eachNum===0){
+            zero++;
+        }
+        if(eachNum>0){
+            positive++;
+        }
+        if(eachNum<0){
+            negative++;
+        }
+    }
+    console.log((positive/arr.length).toFixed(6))
+    console.log((negative/arr.length).toFixed(6))
+    console.log((zero/arr.length).toFixed(6))
+
+
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    plusMinus(arr);
+}
