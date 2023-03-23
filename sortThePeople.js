87898bf06d56ed75cec95edf1a627fdc9d77a2d1
dahn0209 <dahn0209@gmail.com>
2023-03-22 22:08:26 -0400

    more commit

--- sortThePeople.js
+++ sortThePeople.js
@@ -26,3 +26,34 @@ var sortPeople = function(names, heights) {
     }
     return (result)
 };
+
+
+
+/**
+ * @param {string[]} names
+ * @param {number[]} heights
+ * @return {string[]}
+ */
+var sortPeople = function(names, heights) {
+
+    let obj={};
+    
+    for(let i=0;i<heights.length;i++){
+
+        if(!obj[i]){
+            obj[i]=heights[i]
+        }
+    }
+    let objArr=(Object.entries(obj))
+
+    objArr.sort((a,b)=>b[1]-a[1]);
+
+    objArr=objArr.map(eachPerson=>{
+        let eachId=parseInt(eachPerson[0])
+        eachPerson=names[eachId]
+        return eachPerson
+    })
+    
+    return objArr
+
+};
