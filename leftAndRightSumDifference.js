3aea0088e8963771714036c2d38ce99457301b0b
dahn0209 <dahn0209@gmail.com>
2023-04-06 03:34:36 -0400

    changes

--- leftAndRightSumDifference.js
+++ leftAndRightSumDifference.js
@@ -28,3 +28,32 @@ var leftRigthDifference = function(nums) {
     }
     return result    
 };
+
+var leftRigthDifference = function(nums) {
+
+    let leftSum=[0];
+    let rightSum=[0];
+
+    for(let i=0;i<nums.length;i++){
+  
+        let newSum=leftSum[i]+nums[i];
+        leftSum.push(newSum) 
+    }
+
+    for(let j=nums.length-1;j>=0;j--){
+   
+        let newSum=nums[j]+rightSum[0];
+        rightSum.unshift(newSum);
+    }
+
+    newLeftSum=leftSum.slice(0,nums.length);
+    newRightSum=rightSum.slice(-nums.length);
+    let result=[]
+    
+    for(let i=0;i<nums.length;i++){
+
+        let newVal=Math.abs(newLeftSum[i]-newRightSum[i]);
+        result.push(newVal)
+    }
+    return(result)
+};
