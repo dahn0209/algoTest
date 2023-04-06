f5581271fe7810861c35dc7f178a612215129d44
dahn0209 <dahn0209@gmail.com>
2023-04-06 17:21:49 -0400

    changes more

--- numberOfGoodPairs.js
+++ numberOfGoodPairs.js
@@ -73,3 +73,22 @@ var numIdenticalPairs = function(nums) {
 
     return result    
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function(nums) {
+
+    let count=0;
+
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j,j<nums.length;j++){
+            if(nums[i]===nums[j]){
+                count++;
+            }
+        }
+    }  
+
+    return count 
+};
