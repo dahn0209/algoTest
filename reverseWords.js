d76b4af65abe983f4d97a42f93e2839706e9f98a
dahn0209 <dahn0209@gmail.com>
2023-05-07 02:12:05 -0400

    reverse Words

---
+++ reverseWords.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+  
+    let arr=s.split(' ');
+  
+    let reverseStr='';
+  
+    for(let i=arr.length-1;i>=0;i--){
+        if(arr[i]==='') continue;
+        if(reverseStr.length>0){
+            reverseStr+=' ';
+        }
+        reverseStr+=arr[i]
+    }
+      return(reverseStr)
+  };
