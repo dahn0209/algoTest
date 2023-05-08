a72f05bd365e75a2c79b9ac3d1457c6079a76395
dahn0209 <dahn0209@gmail.com>
2023-05-07 21:26:56 -0400

    changes

--- mergeAlternatively.js
+++ mergeAlternatively.js
@@ -16,3 +16,29 @@ var mergeAlternately = function(word1, word2) {
     return(result)
     
 };
+
+
+var mergeAlternately = function(word1, word2) {
+
+    let min=Math.min(word1.length,word2.length);
+
+    let result=''
+
+    for(let i=0;i<min;i++){
+
+            result+=word1[i];
+        
+            result+=word2[i]
+    
+    }
+
+    if(word1.length===min){
+        return(result+=word2.substring(min))
+    }
+
+    else if(word2.length===min){
+        return (result+=word1.substring(min))
+    }
+
+    
+};
