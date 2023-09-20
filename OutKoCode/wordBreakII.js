43a1eb55fb5f7dec3aff309e9ea181bdc6bae510
dahn0209 <dahn0209@gmail.com>
2023-09-19 22:55:33 -0400

    another word break II

--- OutKoCode/wordBreakII.js
+++ OutKoCode/wordBreakII.js
@@ -22,3 +22,32 @@ var wordBreak = function(s, wordDict,memo={}) {
  memo[s] = output
  return output
 };
+
+
+
+var wordBreak = function(s, wordDict) {
+    
+    let memo={}
+       // Return already solved for sentance
+    if(memo[s]!==undefined){ 
+        return memo[s]
+    }
+    // Base case of yet unsolved sentance
+    if(s.length===0) return ['']
+
+    const output = []
+
+    for(const word of wordDict) {
+        if(s.indexOf(word)===0) {
+            const suffix = s.slice(word.length)
+            // All different sentances to make the suffix
+            const suffixWays = wordBreak(suffix, wordDict, memo)
+            // Add the word to all the different sentance combinations
+            const targetWays = suffixWays.map(way => word + (way.length ? ' ' : '') + way)
+            output.push(...targetWays)
+        }
+    }
+
+    memo[s] = output
+    return output
+};
