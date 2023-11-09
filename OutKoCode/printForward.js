34749c20e1f92ecca266850386456e4b15037e48
dahn0209 <dahn0209@gmail.com>
2023-11-09 01:41:35 -0500

    another node

--- OutKoCode/printForward.js
+++ OutKoCode/printForward.js
@@ -17,12 +17,90 @@
 
 function printForward(node) {
     // Write your code here
-    let current=node;
+    let current=node; ////thhis will help show the current node//
     
     while(current!==null){
         console.log(current.data)
         current=current.next;
     }
     
+
+}
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+
+/*
+ * Complete the 'printForward' function below.
+ *
+ * The function accepts INTEGER_SINGLY_LINKED_LIST node as parameter.
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+
+function printForward(node) {
+    // Write your code here
+    
+    while(node!==null){
+        
+        console.log(node.data)
+        node=node.next;
+    }
+    
+
 
 }
